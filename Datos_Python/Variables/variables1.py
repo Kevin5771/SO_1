b2049c74f3938a5c8298fd70c8e0c54552cb593e
Kevin5771 <kevin577gutierrez@gmail.com>
2025-07-30 20:43:45 -0600

    Versión 1: "VARIABLES"

---
+++ Datos_Python/Variables/variables1.py
@@ -0,0 +1,21 @@
+# Programa que demuestra la manipulación básica de variables en Python
+
+# Declaración e inicialización de variables
+nombre = "Juan"
+edad = 25
+altura = 1.75
+es_estudiante = True
+
+# Imprimir información utilizando variables
+print("Nombre:", nombre)
+print("Edad:", edad, "años")
+print("Altura:", altura, "metros")
+print("¿Es estudiante?", es_estudiante)
+
+# Realizar operaciones con variables
+nueva_edad = edad + 5
+altura_doble = altura * 2
+
+# Imprimir resultados de operaciones
+print("Edad después de 5 años:", nueva_edad, "años")
+print("Altura duplicada:", altura_doble, "metros")
---
+++ Datos_Python/Variables/variables2.py
@@ -0,0 +1,25 @@
+# Programa que demuestra el uso de variables en operaciones matemáticas y manipulación de cadenas en Python
+
+# Declaración e inicialización de variables
+numero1 = 10
+numero2 = 5
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+# Imprimir resultados de operaciones matemáticas
+print("Suma:", suma)
+print("Resta:", resta)
+print("Multiplicación:", multiplicacion)
+print("División:", division)
+
+# Declaración de variables de cadena
+saludo = "Hola, "
+nombre = "Ana"
+
+# Concatenación de cadenas
+mensaje_completo = saludo + nombre
+
+# Imprimir mensaje completo
+print(mensaje_completo)
